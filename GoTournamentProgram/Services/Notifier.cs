f3921d8ee47a6ca27385abb85b5348b943f8a752
timofey1198 <timostar98@gmail.com>
2018-05-09 22:40:28 +0300

    Класс, реализующий интерфейс INotifyPropertyChanged

---
+++ GoTournamentProgram/Services/Notifier.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace GoTournamentProgram.Services
+{
+    /// <summary>
+    /// Реализует интерфейс INotifyPropertyChanged
+    /// </summary>
+    [DataContract]
+    public class Notifier : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Реализация интерфейса INotifyPropertyChanged
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
