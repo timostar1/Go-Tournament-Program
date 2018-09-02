016dc7b61d1dca5c0cb4793e705907eda5043650
antonsluch <asluch@nes.ru>
2018-09-02 12:32:43 +0300

    вью в отдельной папке, папка для стилей. Файл с цветовыми стилями

--- GoTournamentProgram/Views/MainWindow.xaml.cs
+++ GoTournamentProgram/Views/MainWindow.xaml.cs
@@ -25,21 +25,8 @@ namespace GoTournamentProgram
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext =
-                new TournamentVM(new DefaultDialogService(), new JsonFileService());
-        }
-
-        private void PlayerName_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                this.Title = "xxx";
-                RoutedEventArgs ev = new RoutedEventArgs();
-                ev.RoutedEvent = Button.ClickEvent;
-                this.AddPlayerBtn.RaiseEvent(ev);
-                //this.AddPlayerBtn.
-                
-            }
+            this.DataContext = new TournamentVM(
+                new DefaultDialogService(), new JsonFileService(), new EnvironmentService());
         }
     }
 }
